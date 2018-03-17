5eb98804ec0985906d502b8598a96831d20a29ef
Masein Mody <maseinmody@nobody.invalid>
2018-03-17 00:22:23 -0700

    Initial Commit

---
+++ TableAndSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableAndSlider
+//
+//  Created by Masein Mody on 3/17/18.
+//  Copyright © 2018 Masein Mody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
