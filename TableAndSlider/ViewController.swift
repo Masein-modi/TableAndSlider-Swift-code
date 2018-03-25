2e4cf21db3cf9ab63efad63c1586bc211878d49a
masein <dan.hosein@yahoo.com>
2018-03-25 01:15:13 -0700

    happy new year :))

--- TableAndSlider/ViewController.swift
+++ TableAndSlider/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+  
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    @IBAction func sliderChanged(_ sender: Any) {
+        table.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "prototypeCell")
+        //text label returns label which is an optional value
+        cell.textLabel?.text = String((indexPath.row ) * Int(slider.value * 10))
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
